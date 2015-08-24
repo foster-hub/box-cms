ef249bbd367e4d42a2805f768a014731e9b91814
marcos-foster <marcos.dobay@foster.com.br>
2015-08-24 13:30:14 -0300

    criar estrutura inicial para tela #1312.74.246586#

---
+++ Box.CMS/Controllers/CMS_CrossLinksCrontroller.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.Composition;
+using Box.Composition;
+using Box.Composition.Web;
+
+
+namespace Box.CMS.Controllers {
+
+    [Export]
+    [PartCreationPolicy(CreationPolicy.NonShared)]
+    public class CMS_CrossLinksController : Controller {
+
+        [Import]
+        private IPageModel PageModel { get; set; }
+
+        [Import]
+        private CMS.Services.CMSService cms { get; set; }
+
+        [Import]
+        private Core.Services.SecurityService security { get; set; }
+
+        [Authorize(Roles="ADM_CMS")]
+        public ActionResult Index() {
+
+            return View(PageModel);
+        }
+
+    }
+}
---
+++ Box.CMS/Menus/CrossLinksLink.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Box.Composition;
+using System.ComponentModel.Composition;
+
+namespace Box.CMS.Menus {
+
+    [Export(typeof(IMenuActionLink))]
+    public class CrossLinksLink : IMenuActionLink {
+
+        [Import]
+        private Groups.ADM_CMS ADM_CMS { get; set; }
+
+        public string Name {
+            get { return SharedStrings.Cross_Links; }
+        }
+
+        public string ActionLink {
+            get { return "~/cms_crosslinks"; }
+        }
+
+        public string MenuLocal {
+            get { return "TOP"; }
+        }
+
+
+        public string[] Requires {
+            get {
+                return new string[] { ADM_CMS.UserGroupUId };
+            }
+        }
+
+        public int DisplayOrder {
+            get {
+                return 1;
+            }
+        }
+
+    }
+
+}
