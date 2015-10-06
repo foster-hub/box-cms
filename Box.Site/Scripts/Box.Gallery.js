58cf58b656329d2a48b53fb19ad6102c200c071a
venatci <renato.venatci@gmail.com>
2015-10-06 13:26:37 -0300

    #ver porque não esta andando o scroll dos thumbs [BOX] #1673.13.248016#

--- Box.Site/Scripts/Box.Gallery.js
+++ Box.Site/Scripts/Box.Gallery.js
@@ -3,7 +3,6 @@
 Box.Gallery = function (galleryId, nThumbs, height, images, appUrl) {
 
     var me = this;
-
     me.images = images;
     me.galleryId = '__boxImgGallery' + galleryId;
     me.height = height;        
@@ -11,11 +10,8 @@ Box.Gallery = function (galleryId, nThumbs, height, images, appUrl) {
 
     if ($('#' + me.galleryId).length == 0)
         return;
-
     Box.Gallery.siteUrl = appUrl;
-
-    
-    
+        
     var parent = $('#' + me.galleryId).parent();
     var anchor = $('#' + me.galleryId);
     me.maxwidth = parent.width() - anchor.css('padding-left').replace('px', '') - anchor.css('padding-right').replace('px', '');
@@ -37,33 +33,31 @@ Box.Gallery = function (galleryId, nThumbs, height, images, appUrl) {
                 }
         htmlGallery = htmlGallery + '</div>';
 
-        htmlGallery = htmlGallery + '<a class="left carousel-control" href="#imageGallerySlide_' + galleryId + '" role="button" data-slide="prev">';
+        htmlGallery = htmlGallery + '<a class="left carousel-control" href="#imageGallerySlide_' + galleryId + '" role="button" data-slide="prev" onclick="javascript:setTimeout(function () { gallery.carouselMoving(this); }, 1000);">';
         htmlGallery = htmlGallery + '<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>';
         htmlGallery = htmlGallery + '<span class="sr-only">Previous</span>';
         htmlGallery = htmlGallery + '</a>';
-        htmlGallery = htmlGallery + '<a class="right carousel-control" href="#imageGallerySlide_' + galleryId + '" role="button" data-slide="next">';
+        htmlGallery = htmlGallery + '<a class="right carousel-control" href="#imageGallerySlide_' + galleryId + '" role="button" data-slide="next" onclick="javascript:setTimeout(function () { gallery.carouselMoving(this); }, 1000);">';
         htmlGallery = htmlGallery + '<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>';
         htmlGallery = htmlGallery + '<span class="sr-only">Next</span>';
         htmlGallery = htmlGallery + '</a>';
         
         htmlGallery = htmlGallery + '</div>';
 
-        htmlGallery = htmlGallery + '<ol class="carousel-indicators">';
+
+        htmlGallery = htmlGallery + '<div style="overflow:hidden;">';
+        htmlGallery = htmlGallery + '<ol id="imageGalleryIndicators_' + galleryId + '" class="carousel-indicators">';
         for (var image in images) {         
-            htmlGallery = htmlGallery + '<li data-target="#imageGallerySlide_' + galleryId + '" data-slide-to="' + image + '" class="item ' + (images[image] == images[0] ? 'active' : '') + '">';
+            htmlGallery = htmlGallery + '<li data-target="#imageGallerySlide_' + galleryId + '" data-slide-to="' + image + '" class="item ' + (images[image] == images[0] ? 'active' : '') + '" onclick="javascript:setTimeout(function () { gallery.carouselMoving(this); }, 1000);">';
             htmlGallery = htmlGallery + '<img src="' + Box.Gallery.siteUrl + 'files/' + images[image].Folder + '/' + images[image].FileUId + '/?maxHeight=100&height=0&asThumb=false&width=' + thumbWidth + '&maxWidth=0" alt="' + images[image].FileName + '" title="' + images[image].FileName + '">';
             htmlGallery = htmlGallery + "</li>";
         }                        
         htmlGallery = htmlGallery + '</ol>';
-
+        htmlGallery = htmlGallery + '</div>';
 
         htmlGallery = htmlGallery + '</div>';
 
         $('#' + me.galleryId).replaceWith(htmlGallery);
-
-        $('#' + me.galleryId + ' .carousel-control, #' + me.galleryId + ' .__boxImgGallery .carousel-indicators').click(function () {
-            setTimeout(function () { me.carouselMoving(this); }, 1000);
-        });
 
         // thumb jump size
         var allThumbsWidth = me.images.length * thumbWidth;
@@ -71,13 +65,15 @@ Box.Gallery = function (galleryId, nThumbs, height, images, appUrl) {
     }
 
     this.carouselMoving = function (element) {
-        var indexImg = $('#' + me.galleryId + ' .carousel-inner div.active').index();
+        var indexImg = $('#imageGallerySlide_' + galleryId + ' .carousel-inner div.active').index();
         var jump = me.nextSizeThumb * (indexImg + 1);
-        $('#' + me.galleryId + ' .carousel-indicators').css('margin-left', jump * -1 + 'px');
-    };
+        if (indexImg == 0) jump = 0;
 
-    me.create();
+        $('#imageGalleryIndicators_' + galleryId + '.carousel-indicators').css('margin-left', jump * -1 + 'px');
+    };
+    me.create();    
 }
+
 
 Box.Gallery.showFullImage = function (imgFolder, imgId) {
     var lightBox = $('#__boxLightBox');
@@ -86,3 +82,4 @@ Box.Gallery.showFullImage = function (imgFolder, imgId) {
     $('#__boxLightBox img').attr('src', Box.Gallery.siteUrl + 'files/' + imgFolder + '/' + imgId + '?asThumb=false');
     lightBox.show();
 };
+
