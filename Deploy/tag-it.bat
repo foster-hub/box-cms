b5c624182452d561b33b2882e7d81da276322991
marcos-foster <marcos.dobay@foster.com.br>
2015-10-26 17:47:06 -0200

    #3.5 final

--- Deploy/tag-it.bat
+++
@@ -1,6 +0,0 @@
-set SVNBIN=\\foster\data\SVN_BIN\svn
-set SVNPATH=https://github.com/foster-hub/box-cms.git
-@ECHO Taging publish
-del .\pub\box-version.txt
-%SVNBIN% log %SVNPATH% >> .\pub\box-version.txt
-
